4558e85e0e192d0ccb9f42656d07ab69fd5b80ae
Isroilov997 <isroilovmuhammadsolih055@gmail.com>
2025-02-20 08:48:54 +0500

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+ let ism=prompt("Ismingizni kiriting")
+ let familya=prompt("Familyaningizni kiriting")
+ let yosh=prompt("Yoshingizni kiriting")
+let fan = prompt( "Yoqtirgan Fanindiz Yozing" )
+let tuman = prompt( "Yashaydigan tumanindiz Kiriting" )
+
+alert( "Malumatlar ucunrahmat" )
+
+console.log("Sizning ismingiz:"+ism)
+console.log("Sizning familyangiz:"+familya)
+console.log("Sizning yoshingiz:"+yosh)
+console.log("Sizning yaqtirgan faningiz:"+fan
+console.log("Sizning yashaydigan tumaningiz :"+tuman)
+ 
